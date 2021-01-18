f7d41c37167d87583f464ca338c930eb00adfe19
Marlon <marcomora61@gmail.com>
2021-01-18 09:28:20 -0500

    Leyendo valores de múltiples

--- formularios/respuesta.php
+++ formularios/respuesta.php
@@ -26,7 +26,33 @@
           <p>Apellido: <?php  echo $apellido; ?></p>
           <?php } else {
             echo "El apellido es obligatorio";
-          } ?>    
+          } ?>   
+
+          <hr>  
+           <?php // validar chekcbox (singular) ?>
+ 
+           <?php if (isset($_POST['notificaciones'])) {
+             $notificaciones = $_POST['notificaciones'];
+             if ($notificaciones == 'on') {
+               echo 'se ha inscrito correctamente a las notifiaciones';
+             }
+           }
+           ?>
+           
+           <?php //validar array de checkboxes ?>
+
+
+           <?php if(isset($_POST['curso'])){
+             $cursos = $_POST['curso'];
+             echo 'Tus cursos son </br>' ;
+             foreach($cursos as $curso){
+               echo $curso . ' <br/>';
+             }
+           }else {
+             echo 'no has elegido ningun curso';
+           }
+           ?>
+
     </div>
   </body>
 </html>
